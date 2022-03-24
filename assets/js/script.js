17e741d9f9369d4f454a9b38f9ab74bbc89381d0
Guille-Rubio <guillermorubiog@gmail.com>
2022-03-24 21:47:41 +0100

    ADD password safety logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,43 +5,74 @@
 const destination = document.getElementById("passbox");
 const restrictionsBox = document.getElementsByClassName("restrictionsBox")[0];
 
-const passInput = document.getElementById("passwordInput")
+const password = document.getElementById("passwordInput")
 
-passInput.addEventListener("focus",()=>{
+password.addEventListener("focus", () => {
     restrictionsBox.classList.remove("display-none")
 })
 
-passInput.addEventListener("input", () => {
+password.addEventListener("input", () => {
     const lowerCaseRegex = /[a-z]+/
     const upperCaseRegex = /[A-Z]+/
     const numRegex = /[0-9]+/
+    const hasLowerCase = lowerCaseRegex.test(password.value)
+    const hasUpperCase = upperCaseRegex.test(password.value)
+    const hasNumber = numRegex.test(password.value)
+
 
     lengthRestriction = document.getElementsByClassName("lengthRestriction")[0]
     caseRestriction = document.getElementsByClassName("caseRestriction")[0]
     numRestriction = document.getElementsByClassName("numRestriction")[0]
 
-    if (passInput.value.length >= 6) {
+    if (password.value.length >= 6) {
         lengthRestriction.style.color = "green"
     } else {
         lengthRestriction.style.color = "gray"
     }
 
-    if (lowerCaseRegex.test(passInput.value) && upperCaseRegex.test(passInput.value)) {
+    if (hasLowerCase && hasUpperCase) {
         caseRestriction.style.color = "green"
     } else {
         caseRestriction.style.color = "gray"
     }
 
-    if (numRegex.test(passInput.value)) {
+    if (hasNumber) {
         numRestriction.style.color = "green"
     } else {
         numRestriction.style.color = "gray"
     }
+
+    const passSafety = [];
+    passSafety.push(password.value.length >= 6, hasLowerCase, hasUpperCase, hasNumber)
+
+    let result = passSafety.filter(test => test == true)
+
+    console.log(result)
+
+    if (result.length > 3) {
+        console.log("all good")
+
+    } else if (result.length > 2) {
+        console.log("three ok")
+
+    } else if (result.length > 1) {
+        console.log("two ok")
+
+    } else if (result.length > 0) {
+        console.log("one ok")
+
+    } else {
+        console.log("all wrong")
+    }
+
 })
 
-passInput.addEventListener("focusout", () => {
+
+
+password.addEventListener("focusout", () => {
     restrictionsBox.classList.add("display-none")
-
 })
+
+
 
 
